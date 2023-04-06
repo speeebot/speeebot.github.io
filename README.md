0e13bc67acce607dfd1b8abc0c50b43297a15552
speeebot <speeebot@gmail.com>
2023-04-06 10:13:11 -0400

    vue js boilerplate

--- README.md
+++
@@ -1,1 +0,0 @@
-# nullsec_v2
---
+++ nullsec/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// import the root component NullsecApp from a single-file component.
+import App from './App.vue'
+
+createApp(App).mount('#app')
